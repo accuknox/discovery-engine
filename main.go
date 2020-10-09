eb11bb30a37c4b23691222886908a8dd3615cd49
seungsoo-lee <seungsoo365@gmail.com>
2020-10-09 08:25:40 +0000

    update test func

--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"fmt"
-)
 
-var (
-	Version, Build, Revision string
+	autopol "github.com/seungsoo-lee/knoxAutoPolicy/autodiscovery"
 )
-
-func init() {
-	fmt.Printf(
-		"\n[%s],\n%s (ver.%s, bld.%s, rev.%s)\n",
-		"STRAIT-GATE", "CLI-API server",
-		Version, Build, Revision, // -ldflags -X 옵션으로 넘긴 값들
-	)
-}
 
 func main() {
 	fmt.Println("hello")
+	autopol.TestGenerateNetworkPolicies()
 }
