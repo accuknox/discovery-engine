d079db91302aae78f382cde4a3568dadc6a53726
seungsoo-lee <seungsoo365@gmail.com>
2021-05-31 13:51:21 +0000

    Updated testing code

--- src/libs/clusterHandler.go
+++ src/libs/clusterHandler.go
@@ -352,10 +352,6 @@ func GetPodsFromCluster(cluster types.Cluster) []types.Pod {
 			continue
 		}
 
-		if podCluster.Namespace == "multiubuntu" {
-			log.Info().Msgf("POD %v", podCluster)
-		}
-
 		pod := types.Pod{
 			Namespace: podCluster.Namespace,
 			PodName:   podCluster.PodName,
