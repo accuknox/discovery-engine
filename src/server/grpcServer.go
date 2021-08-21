e84111284d363c56548755a38ede952da96df6d8
Seungsoo Lee <seungsoo365@gmail.com>
2021-08-21 10:23:09 +0000

    Removed configuration svc

--- src/server/grpcServer.go
+++ src/server/grpcServer.go
@@ -2,9 +2,7 @@ package server
 
 import (
 	"context"
-	"encoding/json"
 
-	"github.com/gogo/protobuf/jsonpb"
 	"github.com/rs/zerolog"
 
 	cfg "github.com/accuknox/knoxAutoPolicy/src/config"
@@ -15,7 +13,6 @@ import (
 	sysworker "github.com/accuknox/knoxAutoPolicy/src/systempolicy"
 
 	"github.com/accuknox/knoxAutoPolicy/src/libs"
-	cpb "github.com/accuknox/knoxAutoPolicy/src/protobuf/v1/config"
 	fpb "github.com/accuknox/knoxAutoPolicy/src/protobuf/v1/consumer"
 	wpb "github.com/accuknox/knoxAutoPolicy/src/protobuf/v1/worker"
 	"github.com/accuknox/knoxAutoPolicy/src/types"
@@ -32,104 +29,6 @@ var log *zerolog.Logger
 
 func init() {
 	log = logger.GetInstance()
-}
-
-// =========================== //
-// == Configuration Service == //
-// ========================== //
-
-type configServer struct {
-	cpb.ConfigStoreServer
-}
-
-func (s *configServer) Add(ctx context.Context, in *cpb.ConfigRequest) (*cpb.ConfigResponse, error) {
-	log.Info().Msg("Add config called")
-
-	m := jsonpb.Marshaler{OrigName: true}
-	str, err := m.MarshalToString(in.GetConfig())
-	if err != nil {
-		return &cpb.ConfigResponse{Msg: err.Error()}, err
-	}
-
-	var config types.Configuration
-	if err := json.Unmarshal([]byte(str), &config); err != nil {
-		return &cpb.ConfigResponse{Msg: err.Error()}, err
-	}
-
-	if err := core.AddConfiguration(config); err != nil {
-		return &cpb.ConfigResponse{Msg: err.Error()}, err
-	}
-
-	return &cpb.ConfigResponse{Msg: "ok"}, nil
-}
-
-func (s *configServer) Get(ctx context.Context, in *cpb.ConfigRequest) (*cpb.ConfigResponse, error) {
-	log.Info().Msg("Get config called")
-
-	results, err := core.GetConfigurations(in.GetConfigName())
-	if err != nil {
-		return &cpb.ConfigResponse{Msg: err.Error()}, nil
-	}
-
-	configs := []*cpb.Config{}
-
-	for i := range results {
-		var config cpb.Config
-		if b, err := json.Marshal(&results[i]); err != nil {
-			log.Error().Msg(err.Error())
-			continue
-		} else {
-			if err := json.Unmarshal(b, &config); err != nil {
-				log.Error().Msg(err.Error())
-				continue
-			}
-		}
-
-		configs = append(configs, &config)
-	}
-
-	return &cpb.ConfigResponse{Msg: "ok", Config: configs}, nil
-}
-
-func (s *configServer) Update(ctx context.Context, in *cpb.ConfigRequest) (*cpb.ConfigResponse, error) {
-	log.Info().Msg("Update config called")
-
-	m := jsonpb.Marshaler{OrigName: true}
-	str, _ := m.MarshalToString(in.GetConfig())
-
-	var config types.Configuration
-	if err := json.Unmarshal([]byte(str), &config); err != nil {
-		return &cpb.ConfigResponse{Msg: err.Error()}, err
-	}
-
-	err := core.UpdateConfiguration(in.GetConfigName(), config)
-	if err != nil {
-		return &cpb.ConfigResponse{Msg: err.Error()}, err
-	}
-
-	return &cpb.ConfigResponse{Msg: "ok"}, err
-}
-
-func (s *configServer) Delete(ctx context.Context, in *cpb.ConfigRequest) (*cpb.ConfigResponse, error) {
-	log.Info().Msg("Delete config called")
-
-	err := core.DeleteConfiguration(in.GetConfigName())
-	if err != nil {
-		return &cpb.ConfigResponse{Msg: err.Error()}, nil
-	}
-
-	return &cpb.ConfigResponse{Msg: "ok"}, nil
-}
-
-func (s *configServer) Apply(ctx context.Context, in *cpb.ConfigRequest) (*cpb.ConfigResponse, error) {
-	log.Info().Msg("Apply config called")
-
-	err := core.ApplyConfiguration(in.GetConfigName())
-	if err != nil {
-		return &cpb.ConfigResponse{Msg: err.Error()}, nil
-	}
-
-	return &cpb.ConfigResponse{Msg: "ok"}, nil
 }
 
 // ==================== //
@@ -244,12 +143,10 @@ func GetNewServer() *grpc.Server {
 	reflection.Register(s)
 
 	// create server instances
-	configServer := &configServer{}
 	workerServer := &workerServer{}
 	consumerServer := &consumerServer{}
 
 	// register gRPC servers
-	cpb.RegisterConfigStoreServer(s, configServer)
 	wpb.RegisterWorkerServer(s, workerServer)
 	fpb.RegisterConsumerServer(s, consumerServer)
 
