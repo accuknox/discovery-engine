0fbe3964fde8e39f5a487afc9e402277eac5ddd0
daemon1024 <barun1024@gmail.com>
2021-08-23 16:40:15 +0530

    Fix system tests to support updated service

--- tests/test-scenarios-local.sh
+++ tests/test-scenarios-local.sh
@@ -7,32 +7,34 @@ BLUE='\033[0;34m'
 MAGENTA='\033[0;35m'
 NC='\033[0m'
 
-AUTOPOL_HOME=`dirname $(realpath "$0")`/..
-AUTOPOL_POLICY=$AUTOPOL_HOME/policies
+AUTOPOL_HOME=$(dirname $(realpath "$0"))/..
 AUTOPOL_SRC_HOME=$AUTOPOL_HOME/src
 
-TEST_HOME=`dirname $(realpath "$0")`
-TEST_SCRIPTS=`dirname $(realpath "$0")`/scripts
+TEST_HOME=$(dirname $(realpath "$0"))
+TEST_SCRIPTS=$(dirname $(realpath "$0"))/scripts
 
 PASSED_TESTS=()
 FAILED_TESTS=()
 COUNT_TESTS=0
 
-## ==================== ##   
+## ==================== ##
 ## == KnoxAutoPolicy == ##
 ## ==================== ##
 
 res_start_service=0
 
 function start_and_wait_for_KnoxAutoPolicy_initialization() {
-    $AUTOPOL_HOME/scripts/start_service.sh &> /dev/null &
+    export CONF_FILE_NAME=local
+
+    $AUTOPOL_HOME/src/knoxAutoPolicy -config-path=$AUTOPOL_HOME/tests/conf &
+    echo $?
     if [ $? != 0 ]; then
         res_start_service=1
         exit 1
     fi
 
-    sleep 1
-    
+    sleep 3
+
     LISTEN=$(ps -e | grep knoxAutoPolicy | wc -l)
     if [ $LISTEN != 1 ]; then
         res_start_service=1
@@ -43,28 +45,14 @@ function start_and_wait_for_KnoxAutoPolicy_initialization() {
 function stop_and_wait_for_KnoxAutoPolicy_termination() {
     ps -e | grep knoxAutoPolicy | awk '{print $1}' | xargs -I {} kill {}
 
-    for (( ; ; ))
-    do
-        ps -e | grep knoxAutoPolicy &> /dev/null
+    for (( ; ; )); do
+        ps -e | grep knoxAutoPolicy &>/dev/null
         if [ $? != 0 ]; then
             break
         fi
 
-        sleep 1
+        sleep 3
     done
-}
-
-function replace_discovery_mode() {
-    if [[ $1 == *"_egress_ingress_"* ]]; then
-        sed -i "s/DISCOVERY_POLICY_TYPES=1/DISCOVERY_POLICY_TYPES=3/" $AUTOPOL_HOME/scripts/start_service.sh
-        sed -i "s/DISCOVERY_POLICY_TYPES=2/DISCOVERY_POLICY_TYPES=3/" $AUTOPOL_HOME/scripts/start_service.sh
-    elif [[ $1 == *"_egress_"* ]]; then
-        sed -i "s/DISCOVERY_POLICY_TYPES=2/DISCOVERY_POLICY_TYPES=1/" $AUTOPOL_HOME/scripts/start_service.sh
-        sed -i "s/DISCOVERY_POLICY_TYPES=3/DISCOVERY_POLICY_TYPES=1/" $AUTOPOL_HOME/scripts/start_service.sh
-    else
-        sed -i "s/DISCOVERY_POLICY_TYPES=1/DISCOVERY_POLICY_TYPES=2/" $AUTOPOL_HOME/scripts/start_service.sh
-        sed -i "s/DISCOVERY_POLICY_TYPES=3/DISCOVERY_POLICY_TYPES=2/" $AUTOPOL_HOME/scripts/start_service.sh
-    fi
 }
 
 ## ============== ##
@@ -75,9 +63,8 @@ function start_and_wait_for_mysql_initialization() {
     cd $TEST_HOME/mysql
     docker-compose up -d
 
-    for (( ; ; ))
-    do
-        docker logs mysql-example > ./logs 2>&1
+    for (( ; ; )); do
+        docker logs mysql-example >./logs 2>&1
         log=$(cat $TEST_HOME/mysql/logs)
         if [[ $log == *"Ready for start up"* ]]; then
             break
@@ -107,12 +94,11 @@ function apply_and_wait_for_microservice_creation() {
         return
     fi
 
-    for (( ; ; ))
-    do
+    for (( ; ; )); do
         RAW=$(kubectl get pods -n multiubuntu | wc -l)
 
-        ALL=`expr $RAW - 1`
-        READY=`kubectl get pods -n multiubuntu | grep Running | wc -l`
+        ALL=$(expr $RAW - 1)
+        READY=$(kubectl get pods -n multiubuntu | grep Running | wc -l)
 
         if [ $ALL == $READY ]; then
             break
@@ -136,41 +122,24 @@ function delete_and_wait_for_microserivce_deletion() {
 ## == Test Functions == ##
 ## ==================== ##
 
-function add_label_pods() {
-    if [[ $2 == "TC_16" ]] || [[ $2 == "TC_30" ]]; then
-        cd $1
-        /bin/bash ./add_label.sh $> /dev/null
-    fi
-}
-
-function del_label_pods() {
-    if [[ $2 == "TC_16" ]] || [[ $2 == "TC_30" ]]; then
-        cd $1
-        /bin/bash ./del_label.sh $> /dev/null
-    fi
-}
-
 function run_test_case() {
     cd $1
 
     ACTUAL_YAML_FILE=cilium_policies_$2.yaml
 
-    for JSON_FILE in $(ls -r $TC_*.json)
-    do
-        for EXPECTED_YAML_FILE in $(ls -r $TC_*.yaml)
-        do
-            # check before / after
-            if [[ $JSON_FILE == *"before"* ]] && [[ $EXPECTED_YAML_FILE != *"before"* ]]; then
-                continue
-            elif [[ $JSON_FILE == *"after"* ]] && [[ $EXPECTED_YAML_FILE != *"after"* ]]; then
-                continue
-            fi
-
+    for JSON_FILE in $(ls -r $TC_*.json); do
+        for EXPECTED_YAML_FILE in $(ls -r $TC_*.yaml); do
             echo -e "${GREEN}[INFO] Discovering from $JSON_FILE"
-            if [[ $JSON_FILE == *"after"* ]]; then
-                $TEST_SCRIPTS/startTest.sh $1/$JSON_FILE &> /dev/null
+            cp $1/$JSON_FILE test-flow.json
+
+            # start knoxAutoPolicy
+            echo -e "${ORANGE}[INFO] Starting KnoxAutoPolicy${NC}"
+            start_and_wait_for_KnoxAutoPolicy_initialization
+            if [ $res_start_service != 0 ]; then
+                echo -e "${RED}[FAIL] Failed to start KnoxAutoPolicy${NC}"
+                exit 1
             else
-                $TEST_SCRIPTS/startTest.sh $1/$JSON_FILE clear &> /dev/null
+                echo "[INFO] Started KnoxAutoPolicy"
             fi
 
             if [ $? != 0 ]; then
@@ -181,7 +150,7 @@ function run_test_case() {
             echo "[INFO] Discovered policies from $JSON_FILE"
 
             echo -e "${GREEN}[INFO] Comparing $EXPECTED_YAML_FILE and $ACTUAL_YAML_FILE${NC}"
-            python3 $TEST_SCRIPTS/diff.py $AUTOPOL_POLICY/$ACTUAL_YAML_FILE $1/$EXPECTED_YAML_FILE
+            python3 $TEST_SCRIPTS/diff.py $1/$ACTUAL_YAML_FILE $1/$EXPECTED_YAML_FILE
             if [ $? != 0 ]; then
                 echo -e "${RED}[FAIL] Failed $3${NC}"
                 FAILED_TESTS+=($testcase)
@@ -204,7 +173,8 @@ cd $AUTOPOL_SRC_HOME
 
 if [ ! -f KnoxAutoPolicy ]; then
     echo -e "${ORANGE}[INFO] Building KnoxAutoPolicy${NC}"
-    make clean > /dev/null ; make > /dev/null
+    make clean >/dev/null
+    make >/dev/null
     echo "[INFO] Built KnoxAutoPolicy"
 fi
 
@@ -225,29 +195,14 @@ apply_and_wait_for_microservice_creation $microservice
 if [ $res_microservice == 0 ]; then
     echo "[INFO] Applied $microservice"
 
-    echo "[INFO] Wait for initialization"
-    sleep 30
     echo "[INFO] Started to run testcases"
 
-## Step 4. Run all the test cases
+    ## Step 4. Run all the test cases
     cd $TEST_HOME/$microservice/test-cases
-    for testcase in $(ls -d $TC_*)
-    do
-        add_label_pods $TEST_HOME/$microservice/test-cases/$testcase $testcase
-
+    for testcase in $(ls -d $TC_*); do
+        cd
         # replace configuration
         JSON_FILE=$(ls $TEST_HOME/$microservice/test-cases/$testcase/*.json)
-        replace_discovery_mode $JSON_FILE
-
-        # start knoxAutoPolicy
-        echo -e "${ORANGE}[INFO] Starting KnoxAutoPolicy${NC}"
-        start_and_wait_for_KnoxAutoPolicy_initialization
-        if [ $res_start_service != 0 ]; then
-            echo -e "${RED}[FAIL] Failed to start KnoxAutoPolicy${NC}"
-            exit 1
-        else
-            echo "[INFO] Started KnoxAutoPolicy"
-        fi
 
         # run a test case
         res_case=0
@@ -260,15 +215,17 @@ if [ $res_microservice == 0 ]; then
             ((COUNT_TESTS++))
         fi
 
+        # Clear DB for next policy generation
+        echo "[INFO] Clear DB"
+        grpcurl -plaintext -d '{"req": "dbclear"}' localhost:9089 v1.worker.Worker.Start
+
         # stop knoxAutoPolicy
         echo -e "${ORANGE}[INFO] Stopping KnoxAutoPolicy${NC}"
         stop_and_wait_for_KnoxAutoPolicy_termination
         echo "[INFO] Stopped KnoxAutoPolicy"
-
-        del_label_pods $TEST_HOME/$microservice/test-cases/$testcase $testcase
     done
 
-## Step 6. Delete Microservice
+    ## Step 6. Delete Microservice
     res_delete=0
     echo -e "${ORANGE}[INFO] Deleting $microservice${NC}"
     delete_and_wait_for_microserivce_deletion $microservice
@@ -291,10 +248,9 @@ echo "       - Completed Cases: " $COUNT_TESTS
 echo "       - Passed    Cases: " ${#PASSED_TESTS[@]}
 echo -n "       - Failed    Cases: " ${#FAILED_TESTS[@]}
 
-if (( ${#FAILED_TESTS[@]} > 0 )); then
+if ((${#FAILED_TESTS[@]} > 0)); then
     echo -n " ("
-    for casenumber in ${FAILED_TESTS[@]}
-    do
+    for casenumber in ${FAILED_TESTS[@]}; do
         echo -n " "$casenumber
     done
     echo ")"
