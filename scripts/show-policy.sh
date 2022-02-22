7326127a1866d860c6d48bec91a44b59cbcf856d
Rahul Jadhav <nyrahul@gmail.com>
2022-02-22 20:42:46 +0530

    updated script
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

--- scripts/show-policy.sh
+++ scripts/show-policy.sh
@@ -4,7 +4,6 @@
 # example: ./show-policy.sh -c kabuntu
 
 BASE=`dirname $0`
-SRC="$BASE/../src"
 
 usage()
 {
@@ -24,10 +23,12 @@ function age() {
 parse_args()
 {
 	NS="default"
-	OPTS=`getopt -o c:n: --long container:namespace: -n 'parse-options' -- "$@"`
+	K8S=0
+	OPTS=`getopt -o kc:n: --long container:namespace:k8s -n 'parse-options' -- "$@"`
     eval set -- "$OPTS"
     while true; do
         case "$1" in
+            -k | --k8s ) K8S=1; shift 2;;
             -n | --namespace ) NS="$2"; shift 2;;
             -c | --container ) CONTNAME="$2"; shift 2;;
             -- ) shift; break ;;
@@ -39,13 +40,20 @@ parse_args()
 
 main()
 {
+	SRC="$BASE/../src"
+	[[ $K8S -ne 0 ]] && SRC="." 
 	rm -rf $SRC/kubearmor_policies_*.yaml 2>/dev/null
-	$BASE/convert_sys_policy.sh 2>&1 >/dev/null || exit 1
+	YAMLNAME_FILTER="kubearmor_policies_default_${NS}_${CONTNAME}_.*.yaml"
+	if [ $K8S -ne 0 ]; then
+		curl -s https://raw.githubusercontent.com/accuknox/tools/main/get_discovered_yamls.sh | bash -s -- -f kubearmor --filter "$YAMLNAME_FILTER" 2>&1 >/dev/null || exit 1
+	else
+		$BASE/convert_sys_policy.sh 2>&1 >/dev/null || exit 1
+	fi
 	newf=`ls -1 $SRC/kubearmor_policies_default_${NS}_${CONTNAME}_*.yaml` || exit 1
 
 	prevf="/tmp/previous.yaml"
 	if [ -f $prevf ]; then
-		[[ $(age $prevf) -gt 10 ]] && cp $newf $prevf #override prevf if older than 10sec
+#		[[ $(age $prevf) -gt 10 ]] && cp $newf $prevf #override prevf if older than 10sec
 		diff -q $prevf $newf > /dev/null
 		if [ $? -ne 0 ]; then
 			diff -y $prevf $newf | grep "^ *process" -A 1000 | colordiff
@@ -56,5 +64,12 @@ main()
 	cp $newf $prevf
 }
 
+prerequisites()
+{
+	command -v diff >/dev/null 2>&1 || { echo >&2 "require diff but it's not installed.  Aborting."; exit 1; }
+	command -v colordiff >/dev/null 2>&1 || { echo >&2 "require colordiff but it's not installed.  Aborting."; exit 1; }
+}
+
+prerequisites
 parse_args $*
 main
