73b30e8039fee521bd023e58124bfd8a4e594e2b
Rahul Jadhav <nyrahul@gmail.com>
2022-03-01 17:58:18 +0530

    added usage info to the script
    removed few unnecessary prints
    
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

--- scripts/observe_clear_sys_data.sh
+++ scripts/observe_clear_sys_data.sh
@@ -6,6 +6,23 @@
 ## For observability, use below format. Currently observability does not support fromsource and duration
 ## ./scripts/observe_clear_sys_data.sh --req observe --clustername default --containername wordpress --namespace wordpress-mysql --labels app=mysql
 
+usage() 
+{
+	cat << EOF
+Usage: $0 <options>
+
+Options could be:
+--req [observe|dbclear]
+--clustername <clustername>
+--namespace <namespace>
+--containername <container-name>
+--labels <set-of-labels> ... for e.g. --labels "xyz=123,abc=456"
+--fromsource <binary-path>
+--duration <1m|1h|1w>
+EOF
+	exit 1
+}
+
 OPTS=`getopt -o s: --long req: --long containername: --long clustername: --long namespace: --long labels: --long fromsource: --long duration: -n 'parse-options' -- "$@"`
 eval set -- "$OPTS"
 while true; do
@@ -21,6 +38,7 @@ while true; do
         * ) break ;;
     esac
 done
+[[ "$REQUEST" == "" ]] && echo "request type [observe|dbclear] not found." && usage
 
 DATA='{"request": "'$REQUEST'", "clusterName": "'$CLUSTER_NAME'", "namespace":"'$NAMESPACE'", "containerName":"'$CONTAINER_NAME'", "labels":"'$LABELS'", "fromSource":"'$FROM_SOURCE'", "duration":"'$DURATION'"}'
 
