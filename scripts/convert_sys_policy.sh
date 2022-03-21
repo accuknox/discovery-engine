103b012b1a1350e085e3d8837fa8dc8504a2770d
Eswar Rajan Subramanian <eswar@accuknox.com>
2022-03-21 08:39:45 +0530

    Add clustername,namespace,labels on creating convert_sys_policy script and grpc endpoint
    Signed-off-by: Eswar Rajan Subramanian <eswar@accuknox.com>

--- scripts/convert_sys_policy.sh
+++ scripts/convert_sys_policy.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
-DATA='{"policytype": "system"}'
+## Run script command in below format"
+## ./scripts/convery_sys_policy.sh --clustername default --namespace wordpress-mysql --labels app=mysql
+
+usage() 
+{
+	cat << EOF
+Usage: $0 <options>
+
+Options could be:
+--clustername <clustername>
+--namespace <namespace>
+--labels <set-of-labels> ... for e.g. --labels "xyz=123,abc=456"
+EOF
+	exit 1
+}
+
+OPTS=`getopt -o s: --long clustername: --long namespace: --long labels: -n 'parse-options' -- "$@"`
+eval set -- "$OPTS"
+while true; do
+    case "$1" in
+        --clustername ) CLUSTER_NAME="$2"; shift 2;;
+        --namespace ) NAMESPACE="$2"; shift 2;;
+        --labels ) LABELS="$2"; shift 2;;
+        -- ) shift; break ;;
+        * ) break ;;
+    esac
+done
+##[[ "$REQUEST" == "" ]] && echo "request type [observe|dbclear] not found." && usage
+
+DATA='{"policytype": "system", "clustername": "'$CLUSTER_NAME'", "namespace":"'$NAMESPACE'", "labels":"'$LABELS'"}'
 
 grpcurl -plaintext -d "$DATA" localhost:9089 v1.worker.Worker.Convert
