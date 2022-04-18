45e0fa58d41d7e170780625d26f3b253cef315bb
ErenYeager713 <gourav@accuknox.com>
2022-04-18 16:23:54 +0000

    updated changes

--- src/libs/dbHandler_test.go
+++ src/libs/dbHandler_test.go
@@ -20,6 +20,86 @@ const Unmet = "unmet expectation error: "
 // ==================== //
 
 func TestGetNetworkPolicies(t *testing.T) {
+	// prepare mock mysql
+	_, mock := NewMock()
+
+	specPtr := &types.Spec{}
+	spec, _ := json.Marshal(specPtr)
+
+	flowIDsPrt := &[]string{}
+	flowID, _ := json.Marshal(flowIDsPrt)
+
+	rows := mock.NewRows([]string{
+		"apiVersion",    // str
+		"kind",          // str
+		"flow_ids",      // []byte
+		"name",          // str
+		"cluster_name",  // str
+		"namespace",     // str
+		"type",          // str
+		"rule",          // str
+		"status",        // str
+		"outdated",      // str
+		"spec",          // []byte
+		"generatedTime", // int
+	}).
+		AddRow("", "test", flowID, "", "", "", "", "", "", "", spec, 0)
+
+	mock.ExpectQuery("^SELECT (.+) FROM network_policy*").
+		WillReturnRows(rows)
+
+	results := GetNetworkPolicies(types.ConfigDB{DBDriver: "mysql"}, "", "", "")
+	assert.Equal(t, results[0].Kind, "test")
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf(Unmet+"%s", err)
+	}
+}
+
+func TestInsertNetworkPolicies(t *testing.T) {
+	// prepare mock mysql
+	_, mock := NewMock()
+
+	policy := types.KnoxNetworkPolicy{}
+
+	specPtr := &policy.Spec
+	spec, _ := json.Marshal(specPtr)
+
+	flowIDsPrt := &policy.FlowIDs
+	flowID, _ := json.Marshal(flowIDsPrt)
+
+	prep := mock.ExpectPrepare("INSERT INTO network_policy")
+	prep.ExpectExec().
+		WithArgs(
+			"",     // str
+			"kind", // str
+			flowID, // []byte
+			"",     // str
+			"",     // str
+			"",     // str
+			"",     // str
+			"",     // str
+			"",     // str
+			"",     // str
+			spec,   // []byte
+			0,      // int
+		).WillReturnResult(sqlmock.NewResult(0, 1))
+
+	nfe := []types.KnoxNetworkPolicy{
+		types.KnoxNetworkPolicy{
+			Kind: "kind",
+		},
+	}
+
+	err := InsertNetworkPoliciesToMySQL(types.ConfigDB{DBDriver: "mysql"}, nfe)
+	assert.NoError(t, err)
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf(Unmet+"%s", err)
+	}
+}
+
+func TestGetNetworkPoliciesSQLite(t *testing.T) {
 	// prepare mock sqlite
 	_, mock := NewMock()
 
@@ -56,7 +136,7 @@ func TestGetNetworkPolicies(t *testing.T) {
 	}
 }
 
-func TestInsertNetworkPolicies(t *testing.T) {
+func TestInsertNetworkPoliciesSQLite(t *testing.T) {
 	// prepare mock sqlite
 	_, mock := NewMock()
 
