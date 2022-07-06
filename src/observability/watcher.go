c8f477dac4de00cebffbc8b63ea2af3b77f72715
Eswar Rajan Subramanian <eswar@accuknox.com>
2022-07-06 13:53:43 +0530

    Extract Pod labels from pod watcher
    Signed-off-by: Eswar Rajan Subramanian <eswar@accuknox.com>

--- src/observability/watcher.go
+++ src/observability/watcher.go
@@ -44,6 +44,7 @@ func WatchK8sPods() {
 			decoder := json.NewDecoder(resp.Body)
 			for {
 				event := types.K8sPodEvent{}
+				labels := []string{}
 				if err := decoder.Decode(&event); err == io.EOF {
 					break
 				} else if err != nil {
@@ -54,10 +55,15 @@ func WatchK8sPods() {
 					continue
 				}
 
+				for key, val := range event.Object.Labels {
+					labels = append(labels, key+"="+val)
+				}
+
 				pod := types.Pod{
 					Namespace: event.Object.ObjectMeta.Namespace,
 					PodName:   event.Object.ObjectMeta.Name,
 					IP:        event.Object.Status.PodIP,
+					Labels:    labels,
 				}
 
 				if event.Type == "ADDED" {
