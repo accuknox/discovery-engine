e798ced0fe6d8f20dc23e020319f953b7621d8ad
Eswar Rajan Subramanian <eswar@accuknox.com>
2022-08-06 02:36:03 +0530

    Modify k3s installation command
    Signed-off-by: Eswar Rajan Subramanian <eswar@accuknox.com>

--- k3s/install_k3s.sh
+++ k3s/install_k3s.sh
@@ -3,7 +3,7 @@
 # Copyright 2022 Authors of auto-policy-discovery
 
 # create a single-node K3s cluster
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik --docker" sh -
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik" sh -
 [[ $? != 0 ]] && echo "Failed to install k3s" && exit 1
 
 KUBEDIR=$HOME/.kube
@@ -18,12 +18,12 @@ fi
 
 cp /etc/rancher/k3s/k3s.yaml $KUBEDIR/config 
 
-#for (( ; ; ))
-#do
-#	status=$(kubectl get pods -A -o jsonpath={.items[*].status.phase})
-#	[[ $(echo $status | grep -v Running | wc -l) -eq 0 ]] && break
-echo "wait for initialization"
-sleep 15
-#done
+for (( ; ; ))
+do
+	status=$(kubectl get pods -A -o jsonpath={.items[*].status.phase})
+	[[ $(echo $status | grep -v Running | wc -l) -eq 0 ]] && break
+	echo "wait for initialization"
+	sleep 1
+done
 
 kubectl get pods -A
