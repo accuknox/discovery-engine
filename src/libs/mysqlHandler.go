46a912c1fe995e5fc3ec431369d3ffde1504c5af
Eswar Rajan <89014588+seswarrajan@users.noreply.github.com>
2022-11-16 18:36:11 +0530

    Get summary data from system_summary DB table instead of system_logs table (#592)
    Signed-off-by: Eswar Rajan Subramanian <eswar@accuknox.com>

--- src/libs/mysqlHandler.go
+++ src/libs/mysqlHandler.go
@@ -1543,7 +1543,7 @@ func GetPodNamesMySQL(cfg types.ConfigDB, filter types.ObsPodDetail) ([]string, 
 	var err error
 
 	// Get podnames from system table
-	query := "SELECT pod_name FROM " + TableSystemLogs_TableName + " "
+	query := "SELECT podname FROM " + TableSystemSummarySQLite + " "
 
 	var whereClause string
 	var sysargs []interface{}
@@ -1570,38 +1570,6 @@ func GetPodNamesMySQL(cfg types.ConfigDB, filter types.ObsPodDetail) ([]string, 
 	}
 
 	results, err = db.Query(query+whereClause, sysargs...)
-	if err != nil {
-		log.Error().Msg(err.Error())
-		return nil, err
-	}
-	defer results.Close()
-
-	for results.Next() {
-		var locPodName string
-		if err := results.Scan(
-			&locPodName,
-		); err != nil {
-			return nil, err
-		}
-		resPodNames = append(resPodNames, locPodName)
-	}
-
-	// Get podnames from network table
-	query = "SELECT source_pod_name FROM " + TableNetworkLogs_TableName + " "
-
-	whereClause = ""
-	var nwargs []interface{}
-
-	if filter.Namespace != "" {
-		concatWhereClause(&whereClause, "source_namespace")
-		nwargs = append(nwargs, filter.Namespace)
-	}
-	if filter.Labels != "" {
-		concatWhereClause(&whereClause, "source_labels")
-		nwargs = append(nwargs, filter.Labels)
-	}
-
-	results, err = db.Query(query+whereClause, nwargs...)
 	if err != nil {
 		log.Error().Msg(err.Error())
 		return nil, err
@@ -1746,4 +1714,13 @@ func UpsertSystemSummaryMySQL(cfg types.ConfigDB, sysSummary map[types.SystemSum
 	}
 
 	return nil
+}
+
+func GetSystemSummaryMySQL(cfg types.ConfigDB, filterOptions types.SystemSummary) ([]types.SystemSummary, error) {
+	db := connectMySQL(cfg)
+	defer db.Close()
+
+	res, err := getSysSummarySQL(db, TableSystemSummarySQLite, filterOptions)
+
+	return res, err
 }
