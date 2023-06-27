d6a830ff3c0594c0e7e16e11fb13daf7c4d387bb
Vishnu Soman <78135259+vishnusomank@users.noreply.github.com>
2023-06-27 15:43:31 +0530

    Aggregate summary data before publishing to stream (#755)
    Signed-off-by: Vishnu Soman <vishnu@accuknox.com>

--- src/observability/publisher.go
+++ src/observability/publisher.go
@@ -1,7 +1,9 @@
 package observability
 
 import (
+	"strings"
 	"sync"
+	"time"
 
 	"github.com/accuknox/auto-policy-discovery/src/common"
 	cfg "github.com/accuknox/auto-policy-discovery/src/config"
@@ -27,8 +29,15 @@ func ProcessSystemSummary() {
 
 	if cfg.GetCfgPublisherEnable() {
 		PublisherMutex.Lock()
+		timeVar := time.Now()
+		tempSummarizerMap = getAggregatedSummaryMap(tempSummarizerMap)
+
+		log.Info().Msgf("Events to publish after aggregation: [%v]", len(tempSummarizerMap))
+		count := 0
+
 		// publish summary map in GRPC
 		for ss, sstc := range tempSummarizerMap {
+			count++
 			var locSummary types.SystemSummary = ss
 
 			// update count/time
@@ -41,7 +50,51 @@ func ProcessSystemSummary() {
 			// clear each published entry from data map
 			delete(tempSummarizerMap, ss)
 		}
+		log.Info().Msgf("Published %v events in %v", count, time.Since(timeVar))
 		PublisherMutex.Unlock()
 	}
 	ProcessSystemSummaryWg.Wait()
+}
+
+func getAggregatedSummaryMap(tempSummarizerMap map[types.SystemSummary]types.SysSummaryTimeCount) map[types.SystemSummary]types.SysSummaryTimeCount {
+
+	var fileArr []string
+	log.Info().Msgf("Events before aggregation: [%v]", len(tempSummarizerMap))
+
+	fileSumMap := make(map[types.SystemSummary]types.SysSummaryTimeCount)
+	updatedSummarizerMap := make(map[types.SystemSummary]types.SysSummaryTimeCount)
+	for tss, tsstc := range tempSummarizerMap {
+		if tss.Operation == types.FileOperation {
+			fileArr = append(fileArr, tss.Destination)
+			fileSumMap[tss] = tsstc
+		} else {
+			updatedSummarizerMap[tss] = tsstc
+		}
+	}
+	tempSummarizerMap = updatedSummarizerMap
+
+	aggregatedFilePaths := common.AggregatePaths(fileArr)
+
+	log.Info().Msgf("Aggregated file paths: [%v]", len(aggregatedFilePaths))
+	for ss, sstc := range fileSumMap {
+		for _, path := range aggregatedFilePaths {
+			if strings.HasPrefix(ss.Destination, path.Path) && (len(ss.Destination) == len(path.Path) || ss.Destination[len(strings.TrimSuffix(path.Path, "/"))] == '/') {
+				ss.Destination = path.Path
+				break
+			}
+		}
+
+		if existingSstc, ok := tempSummarizerMap[ss]; ok {
+			existingSstc.Count += sstc.Count
+			if sstc.UpdatedTime > existingSstc.UpdatedTime {
+				existingSstc.UpdatedTime = sstc.UpdatedTime
+			}
+			tempSummarizerMap[ss] = existingSstc
+		} else {
+			tempSummarizerMap[ss] = sstc
+		}
+
+	}
+
+	return tempSummarizerMap
 }
