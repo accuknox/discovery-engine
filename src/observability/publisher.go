ebb8b597f0ad8967a7cb147f420439b30ec0a028
sahil <sahilraja242@gmail.com>
2023-07-05 09:40:51 +0530

    feat(report): sync aggregation PR
    Signed-off-by: sahil <sahilraja242@gmail.com>

--- src/observability/publisher.go
+++ src/observability/publisher.go
@@ -1,6 +1,10 @@
 package observability
 
 import (
+	"strings"
+	"sync"
+	"time"
+
 	"github.com/accuknox/auto-policy-discovery/src/common"
 	cfg "github.com/accuknox/auto-policy-discovery/src/config"
 	"github.com/accuknox/auto-policy-discovery/src/types"
@@ -16,30 +20,84 @@ func ProcessSystemSummary() {
 	tempSummarizerMap := common.MoveMap(SummarizerMap)
 	SummarizerMapMutex.Unlock()
 
-	//var ProcessSystemSummaryWg sync.WaitGroup
+	aggregateSummaryMap(tempSummarizerMap)
 
 	if cfg.GetCfgObservabilityWriteToDB() {
-		//ProcessSystemSummaryWg.Add(1)
-		UpsertSummaryCronJob(tempSummarizerMap)
+		var ProcessSystemSummaryWg sync.WaitGroup
+		ProcessSystemSummaryWg.Add(1)
+		go UpsertSummaryCronJob(tempSummarizerMap, &ProcessSystemSummaryWg)
+		ProcessSystemSummaryWg.Wait()
 	}
 
 	if cfg.GetCfgPublisherEnable() {
 		PublisherMutex.Lock()
+		initTime := time.Now()
+
+		aggregateSummaryMap(tempSummarizerMap)
+
+		log.Info().Msgf("Events to publish after aggregation: [%v]", len(tempSummarizerMap))
+		count := 0
+
 		// publish summary map in GRPC
-		for ss, sstc := range tempSummarizerMap {
-			var locSummary types.SystemSummary = ss
+		for sysSummary, summaryTimeCount := range tempSummarizerMap {
+			count++
+			var locSummary types.SystemSummary = sysSummary
 
 			// update count/time
-			locSummary.Count = sstc.Count
-			locSummary.UpdatedTime = sstc.UpdatedTime
+			locSummary.Count = summaryTimeCount.Count
+			locSummary.UpdatedTime = summaryTimeCount.UpdatedTime
 
 			// publish data to feeder grpc
 			SysSummary.Publish(&locSummary)
 
 			// clear each published entry from data map
-			delete(tempSummarizerMap, ss)
+			delete(tempSummarizerMap, sysSummary)
 		}
+		log.Info().Msgf("Published %v events in %v", count, time.Since(initTime))
 		PublisherMutex.Unlock()
 	}
-	//ProcessSystemSummaryWg.Wait()
+
+}
+
+func aggregateSummaryMap(summaryMap map[types.SystemSummary]types.SysSummaryTimeCount) {
+
+	podFilePaths := make(map[string][]string)
+	aggPodFilePaths := make(map[string][]common.SysPath)
+	fileSummarizerMap := make(map[types.SystemSummary]types.SysSummaryTimeCount)
+
+	log.Info().Msgf("Events before aggregation: [%v]", len(summaryMap))
+	for sysSummary, summaryTimeCount := range summaryMap {
+		if sysSummary.Operation == types.FileOperation {
+			key := sysSummary.PodName + "_" + sysSummary.Source
+			podFilePaths[key] = append(podFilePaths[key], sysSummary.Destination)
+			fileSummarizerMap[sysSummary] = summaryTimeCount
+			delete(summaryMap, sysSummary)
+		}
+	}
+
+	for key, files := range podFilePaths {
+		aggPodFilePaths[key] = common.AggregatePaths(files)
+		log.Info().Msgf("Got %v aggregated file paths for key [%v]", len(aggPodFilePaths[key]), key)
+	}
+
+	for sysSummary, summaryTimeCount := range fileSummarizerMap {
+		key := sysSummary.PodName + "_" + sysSummary.Source
+		files := aggPodFilePaths[key]
+		for _, path := range files {
+			if strings.HasPrefix(sysSummary.Destination, path.Path) && (len(sysSummary.Destination) == len(path.Path) || sysSummary.Destination[len(strings.TrimSuffix(path.Path, "/"))] == '/') {
+				sysSummary.Destination = path.Path
+				break
+			}
+		}
+		if timeCount, ok := summaryMap[sysSummary]; ok {
+			timeCount.Count += summaryTimeCount.Count
+			if summaryTimeCount.UpdatedTime > timeCount.UpdatedTime {
+				timeCount.UpdatedTime = summaryTimeCount.UpdatedTime
+			}
+			summaryMap[sysSummary] = timeCount
+		} else {
+			summaryMap[sysSummary] = summaryTimeCount
+		}
+
+	}
 }
--- src/types/constants.go
+++ src/types/constants.go
@@ -61,4 +61,6 @@ const (
 	// RecommendedPolicyDescriptionAnnotation is the annotation used to store the description of the recommended policy.
 	// This annotation is used to identify the description associated with a policy by kubearmor-client.
 	RecommendedPolicyDescriptionAnnotation = "policies.kyverno.io/description"
+
+	FileOperation = "File"
 )
